02f8b84cd95208efc65656d5338e6eb2626e89b6
Yash Jadhav <yashjadhav1411@rocketmail.com>
2024-01-06 07:38:50 +0530

    add : local storage

--- App.js
+++ App.js
@@ -8,111 +8,30 @@ import BottomTab from './components/BottomTab/BottomTab';
 import AddButton from './components/AddButton/AddButton';
 import Dialog from "react-native-dialog";
 import uuid from 'react-native-uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const TODOLIST = [
-  // {
-  //   id: 1,
-  //   title: "Learn React Native",
-  //   isCompleted: true,
-  // },
-  // {
-  //   id: 2,
-  //   title: "Learn Redux",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 3,
-  //   title: "Learn React Navigation",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 4,
-  //   title: "Learn React Native Paper",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 5,
-  //   title: "Learn React Native Vector Icons",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 6,
-  //   title: "Learn React Native Gesture Handler",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 7,
-  //   title: "Learn React Native Reanimated",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 8,
-  //   title: "Learn React Native Safe Area Context",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 9,
-  //   title: "Learn React Native Tab View",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 10,
-  //   title: "Learn React Native Web View",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 11,
-  //   title: "Learn React Native Modal",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 12,
-  //   title: "Learn React Native Image Picker",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 13,
-  //   title: "Learn React Native Image Crop Picker",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 14,
-  //   title: "Learn React Native Image Editor",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 15,
-  //   title: "Learn React Native Image Filters",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 16,
-  //   title: "Learn React Native Image Marker",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 17,
-  //   title: "Learn React Native Image Resizer",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 18,
-  //   title: "Learn React Native Image Rotate",
-  //   isCompleted: false,
-  // },
-  // {
-  //   id: 19,
-  //   title: "Learn React Native Image Size",
-  //   isCompleted: false,
-  // },
-]
 
+let firstRender = true
 const App = () => {
 
-  const [todoList, setTodoList] = React.useState(TODOLIST)
+  const [todoList, setTodoList] = React.useState([])
   const [selectedTab, setSelectedTab] = React.useState('All')
   const [showDialog, setShowDialog] = React.useState(false)
   const [inputText, setInputText] = React.useState('')
+
+  React.useEffect(() => {
+    getTodoList()
+  }, [])
+  React.useEffect(() => {
+    if (firstRender) {
+      firstRender = false
+      return
+    }
+    saveTodo()
+  }, [todoList])
+
+
+
 
   const renderTodoList = () => {
     return filterTodoList(selectedTab).map((todo) => (
@@ -150,21 +69,53 @@ const App = () => {
     }
     const newTodoList = [...todoList, newTodo]
     setTodoList(newTodoList)
+    setInputText('')
     setShowDialog(false)
   }
 
+  const saveTodo = async () => {
+    console.log('saveTodo')
+    try {
+      await AsyncStorage.setItem('@todoList', JSON.stringify(todoList))
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const getTodoList = async () => {
+    console.log('getTodoList')
+    try {
+      const todoList = await AsyncStorage.getItem('@todoList')
+      if (todoList) {
+        setTodoList(JSON.parse(todoList))
+      }
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+
+
+
   const renderDailog = () => {
+    
     return (
-      <Dialog.Container visible={showDialog} onBackdropPress={()=>setShowDialog(false)}>
+      <Dialog.Container
+       visible={showDialog} onBackdropPress={()=>setShowDialog(false)}>
       <Dialog.Title>Add todo</Dialog.Title>
       <Dialog.Input 
+      textInputRef={(input)=>input && input.focus()}
       onChangeText={(text)=>setInputText(text)}
        placeholder='eg. Learn React Native' />
 
       <Dialog.Button
         color="grey"
        label="Cancel" onPress={()=>setShowDialog(false)} />
-      <Dialog.Button label="Save" onPress={addTodo} />
+      <Dialog.Button
+        style={{opacity: inputText ? 1 : 0.5}}
+        disabled={!inputText}
+       label="Save" onPress={addTodo} />
     </Dialog.Container>
     )
   }
